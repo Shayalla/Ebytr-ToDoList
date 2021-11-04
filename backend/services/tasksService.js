7b7ffd3eea100d9d91af7e59d5ac94777b8e3914
Jessie <miss_sc_jessi@hotmail.com>
2021-11-04 14:53:08 -0300

    add edit and delete

--- backend/services/tasksService.js
+++ backend/services/tasksService.js
@@ -1,35 +1,43 @@
 const Joi = require('joi');
 const tasksModel = require('../models/tasksModel');
 
-const isValid = (task, done) => {
+const isValid = (task, status) => {
   const tasks = Joi.object({
     task: Joi.string().required(),
-    done: Joi.string().required(),
+    status: Joi.string().required(),
   });
-  const { message } = tasks.validate({ task, done }).error.details[0]
+  const { message } = tasks.validate({ task, status }).error.details[0];
   return message;
-}
-
-const findAllTasks = async () => await tasksModel.findAll();
-
-const createTask = async (task, done) => {
-  const err = await isValid(task, done);
-  if (err) return { err, error: true };
-
-  await tasksModel.addTasks(task, done);
-  return { task, done };
 };
 
-const updateTask = async (id, task, done) => {
-  const err = await isValid(task, done);
+const findAllTasks = async () => {
+  const find = await tasksModel.findAll();
+  return find;
+};
+
+const createTask = async (task, status) => {
+  const err = await isValid(task, status);
   if (err) return { err, error: true };
 
-  await tasksModel.updateTask(id, task, done);
-  return { task, done };
+  await tasksModel.addTasks(task, status);
+  return { task, status };
+};
+
+const updateTask = async (id, task, status) => {
+  await tasksModel.updateTask(id, task, status);
+  const find = await tasksModel.findById(id);
+  return find;
+};
+
+const deleteTask = async (id) => {
+  await tasksModel.deleteTask(id);
+  const message = 'Deletado com sucesso';
+  return message;
 };
 
 module.exports = {
   createTask,
   findAllTasks,
   updateTask,
-}
+  deleteTask,
+};
